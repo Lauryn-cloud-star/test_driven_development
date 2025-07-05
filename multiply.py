ceadfcac102a4f1f7ae58260d8dd1a50fb6dbd92
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-07-05 16:08:46 +0300

    run first test

--- multiply.py
+++ multiply.py
@@ -1,13 +1,2 @@
-def multiply_v1(a, b):
-    return 1
 
-def multiply_v2(a, b):
-    # Base case
-    if b == 0:
-        return 0
 
-    # Handle negative numbers
-    if b < 0:
-        return -multiply_v2(a, -b)
-
-    return a + multiply_v2(a, b - 1)
--- tests/multiply_test.py
+++ tests/multiply_test.py
@@ -1,20 +1,9 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from multiply import multiply_v2
+from multiply import multiply
 
-def test_multiply_1_1():
-    assert multiply_v2(1, 1) == 1
+def test_multilpy_firstcycle():
+    assert multiply(1, 1) == 1
 
 
-def test_multiply_2_2():
-    assert multiply_v2(2, 2) == 4
-
-def test_multiply_3_3():
-    assert multiply_v2(3, 3) == 9
-
-def test_multiply_4_4():
-    assert multiply_v2(4, 4) == 16
-
-def test_multiply_23_45():
-    assert multiply_v2(23, 45) == 1035
