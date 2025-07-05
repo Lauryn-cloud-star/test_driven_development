2b5324d6515578c237c9881d98d0f81be9f371dc
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-07-05 16:14:06 +0300

    run first cycle green

--- multiply.py
+++ multiply.py
@@ -1,2 +1,9 @@
+def multiply_1 (a, b):
+    return 1
 
+# the function that carries out the multiplication
+# this is a simple function that multiplies two numbers
+
+def multiply(a, b):
+    return a * b
 
--- tests/multiply_test.py
+++ tests/multiply_test.py
@@ -3,7 +3,9 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from multiply import multiply
 
-def test_multilpy_firstcycle():
+def test_multiply_firstcycle():
     assert multiply(1, 1) == 1
+
+
 
 
