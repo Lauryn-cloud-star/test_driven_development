48a76923e16fbe29d0d6b9049bef537335493a8d
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-07-05 18:18:56 +0300

    created the test_factorial function for 0

---
+++ tests/test_factorial.py
@@ -0,0 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from factorial import factorial
+
+def test_factorial_for_0():
+    assert factorial(0) == 1
