a7bb90310b93fb86ae5a7fd8a737ce7d9c4fb45b
Lauryn Hope Tuhaise <laurynhope29@gmail.com>
2025-07-09 11:35:27 +0300

    run a test to calculate progressive tax

--- tax.py
+++ tax.py
@@ -5,3 +5,12 @@ def tax(earnings):
         return earnings * 0.20
     else:
         return earnings * 0.40
+    
+
+"""def tax(earnings):
+    if earnings <= 12000:
+        return 0
+    elif earnings <= 36000:
+        return (earnings - 12000) * 0.20
+    else:
+        return (36000 - 12000) * 0.20 + (earnings - 36000) * 0.40"""
